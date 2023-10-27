e7de0811d0e328fd8cf484540a44d09ec0540dcd
Passive <20432486+PassiveModding@users.noreply.github.com>
2023-10-27 20:06:32 +1100

    Remove unused services

--- Meddle/Meddle.Plugin/Service.cs
+++ Meddle/Meddle.Plugin/Service.cs
@@ -16,16 +16,6 @@ public class Service {
 
     [PluginService]
     public static IPluginLog Log { get; private set; } = null!;
-
-    [PluginService]
-    public static IClientState ClientState { get; private set; } = null!;
-
-    [PluginService]
-    public static IDataManager GameData { get; private set; } = null!;
     [PluginService]
     public static IObjectTable ObjectTable { get; private set; } = null!;
-    [PluginService]
-    public static IGameInteropProvider Interop { get; private set; } = null!;
-    [PluginService]
-    public static IGameGui Gui { get; private set; } = null!;
 }
