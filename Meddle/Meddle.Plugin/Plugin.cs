16156191616ba61eb2e2a122f9b9ed9f2a3a9902
Passive <20432486+PassiveModding@users.noreply.github.com>
2024-03-16 16:02:12 +1100

    Only init UI after CS is resolved

--- Meddle/Meddle.Plugin/Plugin.cs
+++ Meddle/Meddle.Plugin/Plugin.cs
@@ -19,10 +19,10 @@ public sealed class Plugin : IDalamudPlugin
 {
     private static readonly WindowSystem WindowSystem = new("Meddle");
     public static readonly string TempDirectory = Path.Combine(Path.GetTempPath(), "Meddle.Export");
-    private readonly MainWindow _mainWindow;
-    private readonly ICommandManager _commandManager;
-    private readonly DalamudPluginInterface _pluginInterface;
-    private readonly IGameInteropProvider _gameInteropProvider;
+    private MainWindow? mainWindow = null;
+    private readonly ICommandManager commandManager;
+    private readonly DalamudPluginInterface pluginInterface;
+    private readonly IGameInteropProvider gameInteropProvider;
 
     [Signature("40 53 48 83 EC 20 FF 81 ?? ?? ?? ?? 48 8B D9 48 8D 4C 24", DetourName = nameof(PostTickDetour))]
     private Hook<PostTickDelegate> postTickHook = null!;
@@ -43,37 +43,34 @@ public sealed class Plugin : IDalamudPlugin
             .AddSingleton<ModelBuilder>()
             .BuildServiceProvider();
         
-        _mainWindow = services.GetRequiredService<MainWindow>();
-        WindowSystem.AddWindow(_mainWindow);
-        
-        _commandManager = services.GetRequiredService<ICommandManager>();
-        _commandManager.AddHandler("/meddle", new CommandInfo(OnCommand)
+        commandManager = services.GetRequiredService<ICommandManager>();
+        commandManager.AddHandler("/meddle", new CommandInfo(OnCommand)
         {
             HelpMessage = "Open the menu"
         });
         
-        _pluginInterface = services.GetRequiredService<DalamudPluginInterface>();
-        _pluginInterface.UiBuilder.Draw += DrawUi;
-        _pluginInterface.UiBuilder.OpenConfigUi += OpenUi;
-        _pluginInterface.UiBuilder.DisableGposeUiHide = true;
+        this.pluginInterface = services.GetRequiredService<DalamudPluginInterface>();
+        this.pluginInterface.UiBuilder.Draw += DrawUi;
+        this.pluginInterface.UiBuilder.OpenConfigUi += OpenUi;
+        this.pluginInterface.UiBuilder.DisableGposeUiHide = true;
 
-        _gameInteropProvider = services.GetRequiredService<IGameInteropProvider>();
+        gameInteropProvider = services.GetRequiredService<IGameInteropProvider>();
         
         // https://github.com/Caraxi/SimpleTweaksPlugin/blob/2b7c105d1671fd6a344edb5c621632b8825a81c5/SimpleTweaksPlugin.cs#L101C13-L103C75
         Task.Run(() =>
         {
             FFXIVClientStructs.Interop.Resolver.GetInstance.SetupSearchSpace(services.GetRequiredService<ISigScanner>().SearchBase);
             FFXIVClientStructs.Interop.Resolver.GetInstance.Resolve();
-            //_gameInteropProvider.InitializeFromAttributes(this);
-            //postTickHook.Enable();
+            
+            services.GetRequiredService<IPluginLog>().Information("Resolved FFXIVClientStructs");
+            mainWindow = services.GetRequiredService<MainWindow>();
+            WindowSystem.AddWindow(mainWindow);
         });
     }
 
     private bool PostTickDetour(nint a1)
     {
         var ret = postTickHook.Original(a1);
-        
-        //NewTree.ProcessQueue();
         return ret;
     }
 
@@ -84,8 +81,10 @@ public sealed class Plugin : IDalamudPlugin
     
     private void OpenUi()
     {
-        _mainWindow.IsOpen = true;
-        _mainWindow.BringToFront();
+        if (mainWindow == null)
+            return;
+        mainWindow.IsOpen = true;
+        mainWindow.BringToFront();
     }
     
     private void DrawUi()
@@ -96,12 +95,11 @@ public sealed class Plugin : IDalamudPlugin
     public void Dispose()
     {
         postTickHook?.Dispose();
-
-        _mainWindow.Dispose();
+        mainWindow?.Dispose();
         WindowSystem.RemoveAllWindows();
-        _commandManager.RemoveHandler("/meddle");
+        commandManager.RemoveHandler("/meddle");
 
-        _pluginInterface.UiBuilder.Draw -= DrawUi;
-        _pluginInterface.UiBuilder.OpenConfigUi -= OpenUi;
+        pluginInterface.UiBuilder.Draw -= DrawUi;
+        pluginInterface.UiBuilder.OpenConfigUi -= OpenUi;
     }
 }
