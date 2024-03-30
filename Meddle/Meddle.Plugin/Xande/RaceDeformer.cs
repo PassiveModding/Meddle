7e7d9145aada17d0a79a0a7ddc1879b9841a6fd5
Passive <20432486+PassiveModding@users.noreply.github.com>
2024-03-30 13:30:24 +1100

    DeformMatrix3x4 struct
    will likely adapt this in future

--- Meddle/Meddle.Plugin/Xande/RaceDeformer.cs
+++ Meddle/Meddle.Plugin/Xande/RaceDeformer.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using System.Text.RegularExpressions;
+using FFXIVClientStructs.FFXIV.Common.Component.BGCollision.Math;
 using Meddle.Plugin.Enums;
 using Meddle.Plugin.Files;
 
@@ -11,7 +12,7 @@ public partial class RaceDeformer(PbdFile pbd, IReadOnlyList<BoneNodeBuilder> bo
     public PbdFile PbdFile { get; } = pbd;
     private IReadOnlyList<BoneNodeBuilder> BoneMap { get; } = boneMap;
 
-    private float[]? ResolveDeformation(PbdFile.Deformer deformer, string name)
+    private PbdFile.DeformMatrix3x4? ResolveDeformation(PbdFile.Deformer deformer, string name)
     {
         // Try and fetch it from the PBD
         var boneNames = deformer.BoneNames;
@@ -31,12 +32,7 @@ public partial class RaceDeformer(PbdFile pbd, IReadOnlyList<BoneNodeBuilder> bo
         }
 
         // No deformation, just use identity
-        return new float[]
-        {
-            0, 0, 0, 0, // Translation (vec3 + unused)
-            0, 0, 0, 1, // Rotation (vec4)
-            1, 1, 1, 0  // Scale (vec3 + unused)
-        };
+        return PbdFile.DeformMatrix3x4.Identity;
     }
 
     /// <summary>Deforms a vertex using a deformer.</summary>
@@ -47,21 +43,9 @@ public partial class RaceDeformer(PbdFile pbd, IReadOnlyList<BoneNodeBuilder> bo
     public Vector3? DeformVertex(PbdFile.Deformer deformer, int nameIndex, Vector3 origPos)
     {
         var matrix = ResolveDeformation(deformer, BoneMap[nameIndex].BoneName);
-        if (matrix != null)
-        {
-            return MatrixTransform(origPos, matrix);
-        }
-
-        return null;
+        return matrix?.TransformCoordinate(origPos);
     }
-
-    // Literally ripped directly from xivModdingFramework because I am lazy
-    private static Vector3 MatrixTransform(Vector3 vector, float[] transform) => new(
-        vector.X * transform[0] + vector.Y * transform[1] + vector.Z * transform[2] + 1.0f * transform[3],
-        vector.X * transform[4] + vector.Y * transform[5] + vector.Z * transform[6] + 1.0f * transform[7],
-        vector.X * transform[8] + vector.Y * transform[9] + vector.Z * transform[10] + 1.0f * transform[11]
-    );
-
+    
     [GeneratedRegex(@"c(?'racecode'\d{4})", RegexOptions.ExplicitCapture | RegexOptions.NonBacktracking)]
     public static partial Regex RaceCodeParser();
 
