b18797986a8b3d37b5b6b762b2a8539ebe28fb34
Passive <20432486+PassiveModding@users.noreply.github.com>
2024-04-27 14:01:22 +1000

    Add shift and slice length options

--- Meddle.UI/Windows/Views/HexView.cs
+++ Meddle.UI/Windows/Views/HexView.cs
@@ -9,15 +9,29 @@ public class HexView(ReadOnlySpan<byte> data)
 
     private int offset;
     private int linesToDraw = 100;
-    public const int SliceLength = 16;
+    private int sliceLength = 16;
+    private int shift = 0;
     public void DrawHexDump()
     {
         var span = this.data.AsSpan();
-        var sliceCount = span.Length / SliceLength;
+        ImGui.SliderInt("Slice Length", ref sliceLength, 8, 32);
+        var sliceCount = span.Length / sliceLength;
         var maxOffset = Math.Max(sliceCount - linesToDraw, 0);
-        ImGui.SliderInt("Offset", ref offset, 0, maxOffset);
+        ImGui.SliderInt("Offset", ref offset, 0, maxOffset); 
+        // +/- for offset since theres a lot of lines
+        ImGui.SameLine();
+        if (ImGui.ArrowButton("##left", ImGuiDir.Left))
+        {
+            offset = Math.Max(offset - 1, 0);
+        }
+        ImGui.SameLine();
+        if (ImGui.ArrowButton("##right", ImGuiDir.Right))
+        {
+            offset = Math.Min(offset + 1, maxOffset);
+        }
         // lines to draw slider
         ImGui.SliderInt("Lines to Draw", ref linesToDraw, 100, 1000);
+        ImGui.SliderInt("Shift", ref shift, 0, sliceLength - 1);
         
         for (var i = offset; i < offset + linesToDraw; i++)
         {
@@ -25,7 +39,8 @@ public class HexView(ReadOnlySpan<byte> data)
             {
                 break;
             }
-            var slice = span.Slice(i * SliceLength, SliceLength);
+            //var slice = span.Slice(i * sliceLength, sliceLength);
+            var slice = span.Slice(i * sliceLength + shift, sliceLength);
             var sliceStr = new StringBuilder();
             var textStr = new StringBuilder();
             for (var j = 0; j < slice.Length; j++)
@@ -53,7 +68,7 @@ public class HexView(ReadOnlySpan<byte> data)
                 textStr.Append(' ');
             }
             
-            var sliceOffset = i * SliceLength;
+            var sliceOffset = i * sliceLength;
             ImGui.TextUnformatted($"{sliceOffset:X8} | {sliceStr} | {textStr}");
             
         }
