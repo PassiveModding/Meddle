32ee85b8193a8508ee1a3112aa50fdedc52658ea
Passive <20432486+PassiveModding@users.noreply.github.com>
2024-07-07 16:46:22 +1000

    Just use characterbase for all non-human modeltypes

--- Meddle/Meddle.Plugin/UI/CharacterTab.cs
+++ Meddle/Meddle.Plugin/UI/CharacterTab.cs
@@ -514,69 +514,35 @@ public unsafe partial class CharacterTab : ITab
                 mdlGroups.ToArray(),
                 skeleton);
         }
-        else if (modelType == CharacterBase.ModelType.DemiHuman)
-        {
-            var demiHuman = (Demihuman*)drawObject;
-            var skeleton = new Skeleton.Skeleton(demiHuman->Skeleton);
-            var customizeParams = new Meddle.Utils.Export.CustomizeParameter();
-            var customizeData = new CustomizeData();
-            var genderRace = GenderRace.Unknown;
-            var mdlGroups = new List<Model.MdlGroup>();
-            foreach (var modelPtr in demiHuman->ModelsSpan)
-            {
-                var model = modelPtr.Value;
-                if (model == null)
-                {
-                    continue;
-                }
-                
-                var mdlGroup = HandleModelPtr(model);
-                if (mdlGroup != null)
-                {
-                    mdlGroups.Add(mdlGroup);
-                }
-            }
-            
-            characterGroup = new ExportUtil.CharacterGroup(
-                customizeParams, 
-                customizeData, 
-                genderRace,
-                mdlGroups.ToArray(),
-                skeleton);
-        }
-        else if (modelType == CharacterBase.ModelType.Monster)
-        {
-            var monster = (Monster*)drawObject;
-            var skeleton = new Skeleton.Skeleton(monster->Skeleton);
-            var customizeParams = new Meddle.Utils.Export.CustomizeParameter();
-            var customizeData = new CustomizeData();
-            var genderRace = GenderRace.Unknown;
-            var mdlGroups = new List<Model.MdlGroup>();
-            foreach (var modelPtr in monster->ModelsSpan)
-            {
-                var model = modelPtr.Value;
-                if (model == null)
-                {
-                    continue;
-                }
-                
-                var mdlGroup = HandleModelPtr(model);
-                if (mdlGroup != null)
-                {
-                    mdlGroups.Add(mdlGroup);
-                }
-            }
-            
-            characterGroup = new ExportUtil.CharacterGroup(
-                customizeParams, 
-                customizeData, 
-                genderRace,
-                mdlGroups.ToArray(),
-                skeleton);
-        }
         else
         {
-            throw new InvalidOperationException($"Unsupported model type: {modelType}");
+            var cbase = (CharacterBase*)drawObject;
+            var skeleton = new Skeleton.Skeleton(cbase->Skeleton);
+            var customizeParams = new Meddle.Utils.Export.CustomizeParameter();
+            var customizeData = new CustomizeData();
+            var genderRace = GenderRace.Unknown;
+            var mdlGroups = new List<Model.MdlGroup>();
+            foreach (var modelPtr in cbase->ModelsSpan)
+            {
+                var model = modelPtr.Value;
+                if (model == null)
+                {
+                    continue;
+                }
+                
+                var mdlGroup = HandleModelPtr(model);
+                if (mdlGroup != null)
+                {
+                    mdlGroups.Add(mdlGroup);
+                }
+            }
+            
+            characterGroup = new ExportUtil.CharacterGroup(
+                customizeParams, 
+                customizeData, 
+                genderRace,
+                mdlGroups.ToArray(),
+                skeleton);
         }
     }
     
