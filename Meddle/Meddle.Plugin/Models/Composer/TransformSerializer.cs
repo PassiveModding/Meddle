4d8a67941b8738ca7f5ea882fe862f289dba4aa4
Passive <20432486+PassiveModding@users.noreply.github.com>
2025-04-05 11:03:17 +1100

    Fix serialization of nan and infinity for transforms

--- Meddle/Meddle.Plugin/Models/Composer/TransformSerializer.cs
+++ Meddle/Meddle.Plugin/Models/Composer/TransformSerializer.cs
@@ -28,13 +28,13 @@ public class TransformSerializer : JsonConverter<Transform>
             switch (propertyName)
             {
                 case "Translation":
-                    translation = new Vector3(reader.GetSingle(), reader.GetSingle(), reader.GetSingle());
+                    translation = new Vector3(reader.GetSingleNamed(), reader.GetSingleNamed(), reader.GetSingleNamed());
                     break;
                 case "Rotation":
-                    rotation = new Quaternion(reader.GetSingle(), reader.GetSingle(), reader.GetSingle(), reader.GetSingle());
+                    rotation = new Quaternion(reader.GetSingleNamed(), reader.GetSingleNamed(), reader.GetSingleNamed(), reader.GetSingleNamed());
                     break;
                 case "Scale":
-                    scale = new Vector3(reader.GetSingle(), reader.GetSingle(), reader.GetSingle());
+                    scale = new Vector3(reader.GetSingleNamed(), reader.GetSingleNamed(), reader.GetSingleNamed());
                     break;
             }
         }
@@ -47,24 +47,81 @@ public class TransformSerializer : JsonConverter<Transform>
         var translation = value.Translation;
         var rotation = value.Rotation;
         var scale = value.Scale;
+        
+        // make writer handle nan and infinity
             
         writer.WriteStartObject();
         writer.WriteStartArray("Translation");
-        writer.WriteNumberValue(translation.X);
-        writer.WriteNumberValue(translation.Y);
-        writer.WriteNumberValue(translation.Z);
+        writer.WriteNumberValueNamed(translation.X);
+        writer.WriteNumberValueNamed(translation.Y);
+        writer.WriteNumberValueNamed(translation.Z);
         writer.WriteEndArray();
         writer.WriteStartArray("Rotation");
-        writer.WriteNumberValue(rotation.X);
-        writer.WriteNumberValue(rotation.Y);
-        writer.WriteNumberValue(rotation.Z);
-        writer.WriteNumberValue(rotation.W);
+        writer.WriteNumberValueNamed(rotation.X);
+        writer.WriteNumberValueNamed(rotation.Y);
+        writer.WriteNumberValueNamed(rotation.Z);
+        writer.WriteNumberValueNamed(rotation.W);
         writer.WriteEndArray();
         writer.WriteStartArray("Scale");
-        writer.WriteNumberValue(scale.X);
-        writer.WriteNumberValue(scale.Y);
-        writer.WriteNumberValue(scale.Z);
+        writer.WriteNumberValueNamed(scale.X);
+        writer.WriteNumberValueNamed(scale.Y);
+        writer.WriteNumberValueNamed(scale.Z);
         writer.WriteEndArray();
         writer.WriteEndObject();
+    }
+}
+
+public static class TransformSerializerFloatExtensions
+{
+    public static float GetSingleNamed(this ref Utf8JsonReader reader)
+    {
+        float value;
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            var str = reader.GetString();
+            if (str == "NaN")
+            {
+                value = float.NaN;
+            }
+            else if (str == "Infinity")
+            {
+                value = float.PositiveInfinity;
+            }
+            else if (str == "-Infinity")
+            {
+                value = float.NegativeInfinity;
+            }
+            else
+            {
+                value = float.Parse(str);
+            }
+        }
+        else
+        {
+            value = reader.GetSingle();
+        }
+
+        return value;
+    }
+    
+    public static void WriteNumberValueNamed(this Utf8JsonWriter writer, float value)
+    {
+        // nan and infinity handling
+        if (float.IsNaN(value))
+        {
+            writer.WriteStringValue("NaN");
+        }
+        else if (float.IsPositiveInfinity(value))
+        {
+            writer.WriteStringValue("Infinity");
+        }
+        else if (float.IsNegativeInfinity(value))
+        {
+            writer.WriteStringValue("-Infinity");
+        }
+        else
+        {
+            writer.WriteNumberValue(value);
+        }
     }
 }
